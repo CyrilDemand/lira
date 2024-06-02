52f725ee1b2d1da9bea78485cb1dfaa1e9dbff70
Cyrildemand <cydemand@gmail.com>
2024-06-02 18:34:20 +0200

    build

--- Assets/Eternal Cat/Cyril/Managers/InventoryManager.cs
+++ Assets/Eternal Cat/Cyril/Managers/InventoryManager.cs
@@ -57,7 +57,6 @@ public class InventoryManager : MonoBehaviour
 
     public void OpenInventory()
     {
-        reArrangeInventory();
         inventaireIsOpen = true;
         panelInventaire.SetActive(true);
         DisplayObject();
@@ -131,59 +130,4 @@ public class InventoryManager : MonoBehaviour
         DisplayObject();
     }
 
-    private void reArrangeInventory()
-    {
-        // Regarde chaque liste, voit s'il y a des items identiques et les regroupe
-        Debug.Log("liste avant : " + inventory.plantes.Count);
-        List<List<Item>> allLists = new List<List<Item>> 
-        { 
-            inventory.plantes.Cast<Item>().ToList(), 
-            inventory.outils.Cast<Item>().ToList(), 
-            inventory.ingredients.Cast<Item>().ToList(), 
-            inventory.potions.Cast<Item>().ToList(), 
-            inventory.cles.Cast<Item>().ToList(), 
-            inventory.quetes.Cast<Item>().ToList() 
-        };
-        foreach (var list in allLists)
-        {
-            var groupedItems = list.GroupBy(item => item.itemName).ToList();
-            Debug.Log("list : " + list.Count);
-            Debug.Log("groupedItems : " + groupedItems.Count);
-            list.Clear();
-            foreach (var group in groupedItems)
-            {
-                Item item = group.First();
-                item.currentStack = group.Sum(i => i.currentStack);
-                if (item.currentStack > item.maxStack)
-                {
-                    int fullStacks = item.currentStack / item.maxStack;
-                    int remainder = item.currentStack % item.maxStack;
-                    for (int i = 0; i < fullStacks; i++)
-                    {
-                        Item newItem = Instantiate(item);
-                        newItem.currentStack = item.maxStack;
-                        list.Add(newItem);
-                    }
-                    if (remainder > 0)
-                    {
-                        Item remainderItem = Instantiate(item);
-                        remainderItem.currentStack = remainder;
-                        list.Add(remainderItem);
-                    }
-                }
-                else
-                {
-                    list.Add(item);
-                }
-            }
-        }
-        Debug.Log("Inventaire réarrangé");
-        DisplayObject();
-        Debug.Log("Inventaire affiché");
-        // afficher toutes les listes
-        foreach (var list in allLists)
-        {
-            Debug.Log("Liste : " + list.Count);
-        }
-    }
 }
